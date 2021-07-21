2e70b646d1ab7983294e976d5562452d2c379042
thankyouvillain <75481409+thankyouvillain@users.noreply.github.com>
2021-07-21 18:29:32 +0900

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
-import profileImage from './img/Software.jpg';
-import './App.css';
+import { Chart } from "react-google-charts";
+import * as React from "react";
+import { render } from "react-dom";
 
-function App() {
+const GoogleCharts = () => {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-      <h2>Author: thankyouvillain</h2>
-      <img src={profileImage} alt="profile-image" />
-    </div>
+    <Chart
+      width={1000} 
+      height={350}
+      chartType="Calendar"
+      loader={<div>Loading Chart</div>}
+      data={[
+        [
+          { type: 'date', id: 'Date' },
+          { type: 'number', id: 'Won/Loss' }
+        ],
+        [new Date(2012, 3, 13), 37032],
+        [new Date(2012, 3, 14), 38024],
+        [new Date(2012, 3, 15), 38024],
+        [new Date(2012, 3, 16), 38108],
+        [new Date(2012, 3, 17), 38229],
+        [new Date(2013, 1, 4), 38177],
+        [new Date(2013, 1, 5), 38705],
+        [new Date(2013, 1, 12), 38210], 
+        [new Date(2013, 1, 13), 38029],
+        [new Date(2013, 1, 19), 38823],
+        [new Date(2013, 1, 23), 38345],
+        [new Date(2013, 1, 24), 38436],
+        [new Date(2013, 2, 10), 38447],
+      ]}
+      options={{
+        title: 'Red Sox Attendance',
+      }}
+      rootProps={{ 'data-testid': '1' }}
+    />
   );
-}
+};
 
-export default App;
+render(<GoogleCharts />, document.querySelector("#root"));
+
